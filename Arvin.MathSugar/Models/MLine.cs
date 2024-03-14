c9caf6167d64f1c829ebef34718b508920dae359
jingshaohui <jingshaohui@ifeng.com>
2024-03-14 18:00:22 +0800

    点模型

--- Arvin.MathSugar/Models/MLine.cs
+++ Arvin.MathSugar/Models/MLine.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Arvin.MathSugar.Models
 {
-    internal class MLine
+    /// <summary>
+    /// 直线/线段
+    /// </summary>
+    public class MLine:ILine<MPoint>
     {
+        public MPoint Start { get; set; }
+        public MPoint End { get; set; }
+        public double Length { get; set; }
+
+        public MLine(MPoint start, MPoint end)
+        {
+            this.Start = start;
+            this.End = end;
+        }
+    }
+
+    /// <summary>
+    /// 直线/线段-数学特征
+    /// </summary>
+    /// <typeparam name="TPoint"></typeparam>
+    public interface ILine<TPoint>
+        where TPoint : IMPoint
+    {
+        TPoint Start { get; set; }
+        TPoint End { get; set; }
+        double Length { get; set; }
     }
 }
--- Arvin.MathSugar/Models/MPoint.cs
+++ Arvin.MathSugar/Models/MPoint.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Arvin.MathSugar.Models
 {
-    internal class MPoint
+    /// <summary>
+    /// 点-几何基础特征
+    /// </summary>
+    public class MPoint: IMPoint
     {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public MPoint(double x,double y,double z)
+        {
+            this.X = x; this.Y = y; this.Z = z;
+        }
+        /// <summary>
+        /// 实现IMPoint接口的点类型均可以直接构造转换
+        /// </summary>
+        /// <param name="point"></param>
+        public MPoint(IMPoint point)
+        {
+            this.X=point.X; this.Y=point.Y; this.Z=point.Z;
+        }
+    }
+    /// <summary>
+    /// 点-数学特征：所有三维空间点均具备的属性
+    /// </summary>
+    public interface IMPoint
+    {
+        double X { get; set; }
+        double Y { get; set; }
+        double Z { get; set; }
     }
 }
