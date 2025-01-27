59bf836fae5e71af82cb509641b962572ea6cc02
Arvin Jing <627539904@qq.com>
2025-01-27 19:06:28 +0800

    #引擎#交易系统

---
+++ Arvin.Extensions/EnumExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Arvin.Extensions
+{
+    /// <summary>
+    /// 枚举扩展
+    /// </summary>
+    public static class EnumExtension
+    {
+        public static string GetDescription(Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+            var attribute = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
+            return attribute == null ? value.ToString() : attribute.Description;
+        }
+    }
+}
